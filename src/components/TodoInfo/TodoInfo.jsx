e9b428565c230feb2f30f1df3ed328ddbd0393d1
vladiksanl <vladislav.liashenko1@gmail.com>
2026-02-18 14:09:05 +0200

    solved

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo = {}, users = [] }) => {
+  const findUser = users.find(u => todo.userId === u.id) || null;
+
+  if (!findUser) {
+    return null;
+  }
+
+  return (
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={findUser} />
+    </article>
+  );
+};
